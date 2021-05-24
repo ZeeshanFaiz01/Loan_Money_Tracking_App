29f4f9553b19dcbc2ae3836183590f12b2a1f095
ZeeshanFaiz01 <mcsf19a029@pucit.edu.pk>
2021-05-24 23:49:39 +0500

    Login

--- Project/app/src/main/java/com/mcsf19a029/loanedmoneytracker/login.java
+++ Project/app/src/main/java/com/mcsf19a029/loanedmoneytracker/login.java
@@ -2,7 +2,9 @@ package com.mcsf19a029.loanedmoneytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signup(View view) {
+        Intent intent = new Intent(this,signup.class);
+        startActivity(intent);
+    }
+
+    public void homePage(View view) {
+        Intent intent = new Intent(this,homepage.class);
+        startActivity(intent);
     }
 }
