28c269213e49d120f31fbc06e53439ac97e12289
ZeeshanFaiz01 <mcsf19a029@pucit.edu.pk>
2021-06-05 01:13:41 +0500

    Recycler

--- Project/app/src/main/java/com/mcsf19a029/loanedmoneytracker/homepage.java
+++ Project/app/src/main/java/com/mcsf19a029/loanedmoneytracker/homepage.java
@@ -1,6 +1,7 @@
 package com.mcsf19a029.loanedmoneytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        RecyclerView recycler = findViewById(R.id.recycler);
     }
 
     public void addRecord(View view) {
